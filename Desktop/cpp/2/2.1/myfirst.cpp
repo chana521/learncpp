6b04e07ded1789f28912961f6125b36aa2986169
邵剑珑 <shaojianlong@shaojianlongdeMacBook-Pro.local>
2019-07-16 17:22:19 +0800

    first

---
+++ Desktop/cpp/2/2.1/myfirst.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+    cout << "Come up and c++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    cin.get();
+    return 0;
+}
